94d7ba5653bba9ded95038c7f3db109030bc840e
Yener Ambrosio <fer22ay@gmail.com>
2020-06-25 17:32:31 -0600

    Cambiando el estilo a la pagina de perl en la rama cabecera

--- changingRoutes.pl
+++ changingRoutes.pl
@@ -6,7 +6,7 @@ use warnings;
 
 get '/toy/:toy_name' => {template => 'toy'} => 'toy';
 get '/meet/:name' => {template => 'staff'} => 'staff';
-get '/' => {template => 'home'} => 'home';
+get '/' => {template => 'home', format=> 'html'} => 'home';
 
 
 app->start;
@@ -20,25 +20,19 @@ __DATA__
 <p>This is <%= ucfirst $name %>.</p>
 
 @@ home.html.ep
+<html>
 <head>
 	<title>Perl Home</title>
-	<link rel="stylesheet" href="css/estilos.css"/>
 </head>
-<body>
-<div id="container">
-	<div id="post">
-		<h1>Este es el titiulo atractivo e interesante del post</h1>
 		<p>Y este es el parrafo de inicio donde vamos a explicar las cosas increibles que se pueden hacer
 		con ramas!</p>
-	</div>
-</div>
-<p>
-	Say hi to <%= link_to 'Santa' => staff => {name => 'santa'} %>
-	and <%= link_to 'Rudolph' => staff => {name => 'rudolph'} %>
-</p>
+	<p>
+		Say hi to <%= link_to 'Santa' => staff => {name => 'santa'} %>
+		and <%= link_to 'Rudolph' => staff => {name => 'rudolph'} %>
+	</p>
 
-<p>
-	And just wait until you see our amazing
-	<%= link_to 'new puzzle' => toy => {toy_name => 'puzzle'} %>!
-</p>
-</body>
+	<p>
+		And just wait until you see our amazing
+		<%= link_to 'new puzzle' => toy => {toy_name => 'puzzle'} %>!
+	</p>
+</html>
