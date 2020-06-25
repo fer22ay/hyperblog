7a4ca37704f5272c1a2a00b89014c544bf83f309
Yener Ambrosio <fer22ay@gmail.com>
2020-06-24 18:35:23 -0600

    Se agrega la nueva version de Debian

--- test-api.pl
+++ test-api.pl
@@ -7,7 +7,7 @@ get '/servers' => sub {
 	return $c->render(
 		json => [
 			{ ip => '10.0.0.1', os => 'Debian 9' },
-			{ ip => '10.0.0.2', os => 'Debian 8' }
+			{ ip => '10.0.0.2', os => 'Debian 10' }
 			],
 		);
 	};
